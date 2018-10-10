5f50a268f5c24a415145b24295c8f334fae7ffaf
Ross Ylitalo <ross.ylitalo@gmail.com>
2018-10-09 20:44:38 -0400

    Added a Page.BottomAppBar (CommandBar) with an AppBarButton to close the application.

--- BigMountainX/TestPage.xaml.cs
+++ BigMountainX/TestPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,11 @@ namespace BigMountainX
         public TestPage()
         {
             this.InitializeComponent();
+        }
+
+        void cmdClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Exit();
         }
     }
 }
